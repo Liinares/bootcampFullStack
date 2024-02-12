c84e2c42424ba1a6c31c5ce1fb1be7ebe95477ea
Liinares <miguellinares908@gmail.com>
2024-02-12 13:50:17 +0100

    ejercicios 1.1 a 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{props.title}</h1>
+  )
 }
 
-export default App;
+const Part = (props) => {
+  return (
+    <p>
+        {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      {props.parts.map((part, index) => {
+        return <Part key={index} name={part.name} exercises={part.exercises} />
+      })}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.exercises}</p>
+  )
+}
+
+const App = () => {
+  let totalExercises = 0;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  for (const part of course.parts) {
+    totalExercises += part.exercises;
+  }
+
+  return (
+    <div>
+      <Header title={course.name}/>
+      <Content parts={course.parts} />
+      <Total exercises={totalExercises} />
+    </div>
+  )
+}
+
+export default App
