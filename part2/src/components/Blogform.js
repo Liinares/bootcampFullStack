144a50279856415970d265b09c6e0046b28c2e7a
Liinares <miguellinares908@gmail.com>
2024-03-07 13:19:40 +0100

    añadiendo el Togglable en el Blogform

--- part2/src/components/Blogform.js
+++ part2/src/components/Blogform.js
@@ -1,37 +1,45 @@
+import { Togglable } from "./Togglable"
+
 const BlogForm = ({handleBlogSubmit, handleLogout, title, setTitle, author, setAuthor, url, setUrl}) => {
     return(
         <div>
-            <h3> Create new Blog</h3>
-            <form onSubmit={handleBlogSubmit}>
-                <input
-                    type="text"
-                    value={title}
-                    name="Title"
-                    placeholder="Title"
-                    onChange={event => setTitle(event.target.value)}
-                />
-                <input
-                    type="text"
-                    value={author}
-                    name="Author"
-                    placeholder="Author"
-                    onChange={event => setAuthor(event.target.value)}
-                />
-                <input
-                    type="text"
-                    value={url}
-                    name="Url"
-                    placeholder="Url"
-                    onChange={event => setUrl(event.target.value)}
-                />
-                <button>
-                    Create new blog
-                </button>
-            </form>
+            <Togglable labelButton={'create blog'}>
+                <div>
+                    <h3> Create new Blog</h3>
+                    <form onSubmit={handleBlogSubmit}>
+                        <input
+                            type="text"
+                            value={title}
+                            name="Title"
+                            placeholder="Title"
+                            onChange={event => setTitle(event.target.value)}
+                        />
+                        <input
+                            type="text"
+                            value={author}
+                            name="Author"
+                            placeholder="Author"
+                            onChange={event => setAuthor(event.target.value)}
+                        />
+                        <input
+                            type="text"
+                            value={url}
+                            name="Url"
+                            placeholder="Url"
+                            onChange={event => setUrl(event.target.value)}
+                        />
+                        <button>
+                            Create new blog
+                        </button>
+                    </form>
+                </div>
+            </Togglable>
             <button onClick={handleLogout}>
                 Cerrar sesión
             </button>
         </div>
+        
+       
     )
 }
 
